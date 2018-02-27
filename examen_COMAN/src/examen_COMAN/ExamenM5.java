35e93c03137417609b93adeacc2198ff8ee26b7d
RyanCooper101 <ryancooper1011@gmail.com>
2018-02-27 09:59:20 +0100

    Examen M5

---
+++ examen_COMAN/src/examen_COMAN/ExamenM5.java
@@ -0,0 +1,17 @@
+package examen_COMAN;
+
+public class ExamenM5 {
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+	public static void printOne() {
+		System.out.println("Hello World !");
+	}
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
+//
